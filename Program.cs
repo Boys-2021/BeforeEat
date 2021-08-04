8373d7f9a6e26729c651ff9947312c82b1791d7c
unknown <adrian.20191176@iit.ac.lk>
2021-08-04 13:28:56 +0530

    Use of Epsilon

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BeforeEat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int x = 0b_101100101;
+            // uint y = 24;
+            // Console.WriteLine(x);
+            // Console.WriteLine(y);
+            // y = y -4294967295;
+            // Console.WriteLine(sizeof(uint)+" "+uint.MinValue+" "+uint.MaxValue);
+            // Console.WriteLine(y);
+            double x = 0.1 + 0.2;
+            Console.WriteLine(x);
+            Console.WriteLine(int.m);         
+        }
+    }
+}
