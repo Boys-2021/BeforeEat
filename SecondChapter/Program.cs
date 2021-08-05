8d7c9408d65e14a01748950f3c1706252f442c83
unknown <adrian.20191176@iit.ac.lk>
2021-08-05 17:00:21 +0530

    Selection Statements

---
+++ SecondChapter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace SecondChapter
+{
+    class Program
+    {
+        static void Main(string[] args){
+        A:
+          Write("What is your height?");
+          string hi = ReadLine();
+
+          hi = hi switch {
+            "a" => "j",
+            "b" =>"Vantingale",
+             _ => "2"
+          };
+          Write(hi);
+      }
+    }
+}
