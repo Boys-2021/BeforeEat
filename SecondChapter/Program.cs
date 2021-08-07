5a4375120c4b632a22a67fa095b72157cf9ea898
unknown <adrian.20191176@iit.ac.lk>
2021-08-07 19:56:08 +0530

    Recursion Function and How to handle overflown

--- SecondChapter/Program.cs
+++ SecondChapter/Program.cs
@@ -5,29 +5,27 @@ namespace SecondChapter
 {
     class Program
     {
-       static string CardinalToOrdinal(int x){
-         switch(x){
-            case 11:
-            case 12:
-            case 13:
-               return ($"{x}th");
-            default:
-               int y = x%10;
-               string res = y switch {
-                  1 => x+"st",
-                  2 => x+"nd",
-                  3 => x+"rd",
-                  _ => x+"th"
-               };
-               return res;
+         static int Factorial(int x ){
+            if(x<1){
+               return 0;
+            }
+            else if(x==1){
+               return 1;
+            }else{
+               checked{
+                return x*Factorial(x-1);
+               }
+            }
          }
-       }
 
-        static void Main(string[] args){
-           for(int i=1; i<41; i++){
-              Write(CardinalToOrdinal(i)+" ");
-           }
-        }
-      }
-    
+         static void Main(string[] args){
+            for(int i=1;i<15;i++){
+               try{
+               WriteLine($"{i}! = {Factorial(i):N0}");
+               }catch{
+                  WriteLine(i+"! Too large Bokka. OVerflown");
+               }
+            }
+         }
+   }
 }
