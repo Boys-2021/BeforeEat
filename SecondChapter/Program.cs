52babf97c3f56e513fc73953f0dc0d2707b132f0
unknown <adrian.20191176@iit.ac.lk>
2021-08-07 19:24:33 +0530

    start of the day

--- SecondChapter/Program.cs
+++ SecondChapter/Program.cs
@@ -6,13 +6,15 @@ namespace SecondChapter
     class Program
     {
         static void Main(string[] args){
-          string input = "";
-          int count = 1;
-          do{
-              WriteLine("Enter a good pass");
-              input = ReadLine();
-              count++;
-          }while(input.Length<10&&count<5);
+         try{
+            string h = ReadLine();
+            WriteLine("I am gonna PArse");
+            int k = int.Parse(h);
+            WriteLine("Parsed SUccessfully");
+         }
+         catch{
+            Write("Fail");
+         }
         }
       }
     
