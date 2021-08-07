94c976832164acdd381f3e1429eca0386a86a000
unknown <adrian.20191176@iit.ac.lk>
2021-08-07 21:27:27 +0530

    debugging

--- SecondChapter/Program.cs
+++ SecondChapter/Program.cs
@@ -5,23 +5,22 @@ namespace SecondChapter
 {
     class Program
     {
-         static int Factorial(int x ){
-            if(x<1){
-               return 0;
-            }
-            else if(x==1){
-               return 1;
-            }else{
-               checked{
-                return x*Factorial(x-1);
-               }
-            }
-         }
+       /// <summary>
+       ///  asdadasd
+       /// </summary>
+       /// <param name="x">asdasd</param>
+       /// <returns>sada</returns>
+         static int Fib(int x ) => 
+            x switch{
+               0 => 0,
+               1 => 1,
+               _ => Fib(x-2)+Fib(x-1)
+            };
 
          static void Main(string[] args){
             for(int i=1;i<15;i++){
                try{
-               WriteLine($"{i}! = {Factorial(i):N0}");
+               WriteLine($"{i}! = {Fib(i):N0}");
                }catch{
                   WriteLine(i+"! Too large Bokka. OVerflown");
                }
