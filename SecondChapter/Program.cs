89bfd00a000e68cf8675defad9a4ccb5210f0775
unknown <adrian.20191176@iit.ac.lk>
2021-08-07 19:46:53 +0530

    Cardinal to Ordinal

--- SecondChapter/Program.cs
+++ SecondChapter/Program.cs
@@ -5,16 +5,28 @@ namespace SecondChapter
 {
     class Program
     {
+       static string CardinalToOrdinal(int x){
+         switch(x){
+            case 11:
+            case 12:
+            case 13:
+               return ($"{x}th");
+            default:
+               int y = x%10;
+               string res = y switch {
+                  1 => x+"st",
+                  2 => x+"nd",
+                  3 => x+"rd",
+                  _ => x+"th"
+               };
+               return res;
+         }
+       }
+
         static void Main(string[] args){
-         try{
-            string h = ReadLine();
-            WriteLine("I am gonna PArse");
-            int k = int.Parse(h);
-            WriteLine("Parsed SUccessfully");
-         }
-         catch{
-            Write("Fail");
-         }
+           for(int i=1; i<41; i++){
+              Write(CardinalToOrdinal(i)+" ");
+           }
         }
       }
     
